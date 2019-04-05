6bc1afabe268f78e812a0901aded779955070722
reVeda <panky2sharma@gmail.com>
2019-04-05 19:49:42 +0530

    Second Commit

---
+++ FacialRecognitionLogin/Constants/AzureConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace FacialRecognitionLogin
+{
+    public static class AzureConstants
+    {
+        //Key1- d7f355817e8b43cdb0ced4773e6dd15e  KEY2-  aa2df1f53de545a785a85626546281a5
+        public const string FacialRecognitionAPIKey = "d7f355817e8b43cdb0ced4773e6dd15e";
+
+        public const string FaceApiBaseUrl = "https://centralindia.api.cognitive.microsoft.com/";
+    }
+}
